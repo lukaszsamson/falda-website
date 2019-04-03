30d728f22f4e8e752aaa4d53458d1736947cbdb7
Lukasz Samson <lukaszsamson@gmail.com>
2019-04-03 11:24:59 +0200

    fix react HMR

---
+++ gatsby-node.js
@@ -0,0 +1,12 @@
+exports.onCreateWebpackConfig = ({
+    getConfig,
+    stage
+}) => {
+    const config = getConfig()
+    if (stage.startsWith('develop') && config.resolve) {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'react-dom': '@hot-loader/react-dom'
+        }
+    }
+}
