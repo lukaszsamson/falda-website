1a54c885662ae4a8c6f9b24474f0cad506c4a644
Lukasz Samson <lukaszsamson@gmail.com>
2019-04-09 18:42:10 +0200

    add about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,24 @@
+require("dotenv").config()
+
 module.exports = {
-    plugins: [{
-        resolve: `gatsby-plugin-typography`,
-        options: {
-            pathToConfigModule: `src/utils/typography`,
-        },
-    }, `gatsby-plugin-sass`],
+  siteMetadata: {
+    title: `Falda`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.DATO_API_TOKEN,
+      },
+    },
+  ],
 }
